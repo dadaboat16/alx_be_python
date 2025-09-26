c9fe735538adfa2b3d5999f85337330316c5467e
dadaboat16 <dadaboat16@gmail.com>
2025-09-26 11:49:01 +0000

    Multiply numbers in sequence

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,5 @@
 number = int(input("Enter a number to see its multiplication table: "))
 
 for variant in range(1, 11):
-    print(number, "*", variant, "=", number * variant) 
+    product = number * variant
+    print(number, "*", variant, "=", product) 
